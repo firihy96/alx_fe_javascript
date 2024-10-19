9da8b6d368750bf7f3dc6efad826e033069dd90b
Abdullah Firihy <abdullahfirihy96@outlook.com>
2024-10-19 18:00:54 +0300

    trial one export functionalty

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -27,7 +27,7 @@ let quoteDisplay = document.getElementById("quoteDisplay"),
   newQuoteCategory = document.getElementById("newQuoteCategory");
 // selecting random quote
 function showRandomQuote() {
-	quotes = [...JSON.parse(window.localStorage.getItem('quotes'))]
+  quotes = [...JSON.parse(window.localStorage.getItem("quotes"))];
   let randomQuoteIndex = randomIntFromInterval(quotes.length);
   let randomQuote = quotes[randomQuoteIndex].text,
     randomQuoteCat = quotes[randomQuoteIndex].category;
@@ -35,10 +35,10 @@ function showRandomQuote() {
 }
 // create quote adding form
 let quoteFormDiv = document.createElement("div"),
-	userInputQuoteText = document.createElement("input"),
-	userInputQuoteCategory = document.createElement("input"),
-	addQuoteBtn = document.createElement("button");
-	addQuoteBtn.textContent = 'Add Quote';
+  userInputQuoteText = document.createElement("input"),
+  userInputQuoteCategory = document.createElement("input"),
+  addQuoteBtn = document.createElement("button");
+addQuoteBtn.textContent = "Add Quote";
 function createAddQuoteForm() {
   // adding attributes
   userInputQuoteText.setAttribute("id", "newQuoteText");
@@ -48,29 +48,54 @@ function createAddQuoteForm() {
   userInputQuoteCategory.setAttribute("type", "text");
   userInputQuoteCategory.setAttribute("placeholder", "Enter quote category");
   addQuoteBtn.setAttribute("onClick", addQuote);
-	//structuring
-	quoteFormDiv.appendChild(userInputQuoteText);
-	quoteFormDiv.appendChild(userInputQuoteCategory);
-	quoteFormDiv.appendChild(addQuoteBtn)
-	document.querySelector('body').appendChild(quoteFormDiv)
+  //structuring
+  quoteFormDiv.appendChild(userInputQuoteText);
+  quoteFormDiv.appendChild(userInputQuoteCategory);
+  quoteFormDiv.appendChild(addQuoteBtn);
+  document.querySelector("body").appendChild(quoteFormDiv);
 }
-
 
 function addQuote() {
   if (!userInputQuoteText.value || !userInputQuoteCategory.value) {
     addQuoteBtn.innerText = "Please, Enter quote";
   } else {
     addQuoteBtn.innerText = "Add Quote";
-    quotes.push({ text: userInputQuoteText.value, category: userInputQuoteCategory.value });
-		// update localStorage 
-		window.localStorage.setItem('quotes', JSON.stringify([...quotes]))
+    quotes.push({
+      text: userInputQuoteText.value,
+      category: userInputQuoteCategory.value,
+    });
+    // update localStorage
+    window.localStorage.setItem("quotes", JSON.stringify([...quotes]));
     userInputQuoteText.value = "";
     userInputQuoteCategory.value = "";
   }
 }
+// Implement JSON Export: Provide a button that allows users to export their quotes to a JSON file. Use Blob and URL.createObjectURL to create a download link.
+
+// downloads the object as 'testJsonFile.json'
+const JSONToFile = (obj, filename) => {
+  const blob = new Blob([JSON.stringify(obj, null, 2)], {
+    type: "application/json",
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `${filename}.json`;
+  a.click();
+  URL.revokeObjectURL(url);
+};
+
 // After parsing html
 document.addEventListener("DOMContentLoaded", () => {
   newQuoteBtn.addEventListener("click", showRandomQuote);
-	createAddQuoteForm()
+  createAddQuoteForm();
   addQuoteBtn.addEventListener("click", addQuote);
+
+  let exportButton = document.createElement("button");
+  exportButton.textContent = "Export Quotes";
+  document.querySelector("body").appendChild(exportButton);
+  exportButton.addEventListener(
+    "click",
+    JSONToFile(...quotes, "Export Quotes")
+  );
 });
